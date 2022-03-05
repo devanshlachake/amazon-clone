761414401308899b3f6f266c1622994aa1d70dbd
Devansh <devansh.docthub@gmail.com>
2022-03-05 15:53:38 +0530

    changes in firebase key

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,12 @@ import 'firebase/compat/firestore';
 
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDvvm0oRFKNUTMPGV5GCJ5vEe1MWbbR4u4",
-  authDomain: "clone-a8a18.firebaseapp.com",
-  projectId: "clone-a8a18",
-  storageBucket: "clone-a8a18.appspot.com",
-  messagingSenderId: "219043349463",
-  appId: "1:219043349463:web:713ae41ee4b6beda2fc192",
-  measurementId: "G-PWK791M2W8"
+  apiKey: "AIzaSyBSaCEunH76X9mQDo-NepPu1kMna3tuimQ",
+  authDomain: "clone-20c87.firebaseapp.com",
+  projectId: "clone-20c87",
+  storageBucket: "clone-20c87.appspot.com",
+  messagingSenderId: "320963326976",
+  appId: "1:320963326976:web:3d42f74fb02ef56bff54e5"
 };
 
 // Use this to initialize the firebase App
